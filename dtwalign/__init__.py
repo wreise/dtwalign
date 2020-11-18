410ab7238b075dc53fc03d1e497c093817b939d4
wreise <wojciech.reise@epfl.ch>
2020-11-18 12:42:09 +0100

    Expose the hidden functions

--- dtwalign/__init__.py
+++ dtwalign/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
-from .dtw import dtw,dtw_from_distance_matrix,dtw_low
+from .dtw import dtw, dtw_from_distance_matrix, dtw_low, _get_pattern
+from .backtrack import _backtrack_jit
